96afe2f027c6b4945d3234c645b67240d4c95ae5
gn-t-k <gntk1995422@gmail.com>
2024-05-22 16:15:30 +0900

    不要なファイル削除

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import sample from "./controller/sample.controller";
 import tasks from "./controller/tasks.controller";
 import "dotenv/config";
 
 const app = new Hono();
 
-app.route("sample", sample);
 app.route("tasks", tasks);
 
 const port = 3000;
--- src/query-service/some-data.query-service.ts
+++
@@ -1,8 +0,0 @@
-export type SomeDataQueryService = () => Promise<Payload>;
-type Payload =
-  | {
-      id: string;
-      required: boolean;
-      number: number;
-    }[]
-  | undefined;
