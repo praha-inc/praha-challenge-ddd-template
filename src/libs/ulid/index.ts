039b373786e37856207c180e3b900ec0c66b4ff0
Karibash <y.karibayashi@gmail.com>
2024-05-27 16:58:24 +0900

    chore: ulidでinsecureを許容せずに、cryptoモジュールを使用するようにする

--- src/libs/ulid/index.ts
+++ src/libs/ulid/index.ts
@@ -1,5 +1,9 @@
-import { detectPrng, factory } from "ulid";
+import crypto from "node:crypto";
+import { factory } from "ulid";
 
-const prng = detectPrng(true);
-
-export const ulid = factory(prng);
+// ulidが内部でrequireを使用している為、ESM形式で出力したJSをNodeで実行時にエラーが発生する
+// import形式でcryptoを読み込み、ulidの内部実装と同じ処理を行うことで解決する
+// @see: https://github.com/ulid/javascript/blob/v2.3.0/lib/index.ts#L133-L134
+export const ulid = factory(() => {
+  return crypto.randomBytes(1).readUInt8() / 0xff;
+});
